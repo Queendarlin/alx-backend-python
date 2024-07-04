00327788de427cf4c6c6dff2642f47a0fa176556
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-04 16:33:29 +0000

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
 """
-Module for a function to zoom in an array-like structure by repeating elements
+Contains a function that returns  a list of integers
+multiplied by certain factor.
 """
+from typing import Tuple, List
 
-from typing import List, Tuple
 
-
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
-    """
-    Zooms in on the elements of the input tuple by repeating each element.
-
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Returns a list of integers multiplied by certain factor.
     Args:
-        lst (Tuple[int, ...]): The input tuple containing integers.
-        factor (int): The factor by which to repeat each element. Default is 2
-
+        lst: A tuple of integers.
+        factor: An integer.
     Returns:
-        List[int]: A list with each element of the input tuple
-        repeated by the given factor.
+        A list of integers.
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
 
 
-array: Tuple[int, ...] = (12, 72, 91)
+array: Tuple = (12, 72, 91)
 
-zoom_2x: List[int] = zoom_array(array)
-zoom_3x: List[int] = zoom_array(array, 3)
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
