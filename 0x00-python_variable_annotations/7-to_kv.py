31ed68479fe5d50d1459d2c0cc941b1021420b99
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-04 12:30:31 +0000

    A a type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple. The first element of the tuple is the string k. The second element is the square of the int/float v and should be annotated as a float.

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module of  a function that returns a tuple
+with a string and the square of an int or float.
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Takes a string k and an int or float v and returns a tuple.
+
+    Args:
+    k (str): A string.
+    v (Union[int, float]): An integer or float.
+
+    Returns:
+    Tuple[str, float]: A tuple containing the string
+    and the square of the int/float.
+    """
+    return (k, float(v ** 2))
