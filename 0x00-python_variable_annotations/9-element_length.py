928f1c2402c33cb0b0ce584ff8eb12a804c52866
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-04 12:43:48 +0000

    Duck type an iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module contains a function to return a list of tuples
+containing elements and their lengths.
+"""
+
+from typing import Iterable, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Takes an iterable of sequences and returns a list of tuples
+    where each tuple contains an element from lst and its length.
+
+    Args:
+    lst (Iterable[Sequence]): An iterable containing sequences
+    (e.g., list, tuple, string).
+
+    Returns:
+    List[Tuple[Sequence, int]]: A list of tuples
+    where each tuple contains an element from lst
+    and its length.
+    """
+    return [(i, len(i)) for i in lst]
