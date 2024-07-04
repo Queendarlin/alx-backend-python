aab4e51c1c6b30ada4f3244cc20873d36e74059f
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-04 11:21:08 +0000

    Using type anotation with concatenation function

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module for concat function with type annotations.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
