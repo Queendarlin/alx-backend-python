7967fcf64d1e31dbcfb2e8c5dba4d365519e99cb
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-04 11:16:49 +0000

    Files added

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Add Function Module using type annotations.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating point numbers
+    :param a: (float): The first number.
+    :param b: b (float): The second number.
+    :return: (float): The sum of a and b.
+    """
+    return a + b
